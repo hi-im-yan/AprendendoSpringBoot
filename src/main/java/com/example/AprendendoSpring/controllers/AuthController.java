da52c4c43a2c4f06c1eed3ea0565ad2dc3730b53
yan ajiki <yan.ajiki@grupocard.com.br>
2021-08-31 10:08:13 -0400

    [feito] endpoint para checar token de autenticação

--- src/main/java/com/example/AprendendoSpring/controllers/AuthController.java
+++ src/main/java/com/example/AprendendoSpring/controllers/AuthController.java
@@ -2,6 +2,7 @@ package com.example.AprendendoSpring.controllers;
 
 import com.example.AprendendoSpring.utils.AuthUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/auth")
-public class LoginController {
+public class AuthController {
 
     @Autowired
     private AuthUtil auth;
@@ -21,6 +22,19 @@ public class LoginController {
         String basicAuth = request.getHeader("FirstAuth");
         if (auth.validaUsuario(basicAuth))
             return basicAuth;
+        return "Unauthorized";
+    }
+
+    @GetMapping("/auth")
+    public String auth(HttpServletRequest request){
+        String basicAuth = request.getHeader("Authorization");
+        try{
+            if (auth.validaUsuario(basicAuth))
+                return "Authorized";
+        }
+        catch(Exception e){
+            return "Unauthorized";
+        }
         return "Unauthorized";
     }
 }
